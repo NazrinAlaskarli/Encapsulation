853be49c25203c2a2156df7a7564de1069087e02
Alaskarli <nazrinna@code.edu.az>
2023-01-30 18:21:14 -0800

    encapsulation

---
+++ Encapsulation/Invoice.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Invoice
+    {
+        private int account;
+        private string customer;
+        private string provider;
+
+
+
+
+        public Invoice(int account,string customer,string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+
+        }
+
+        public string Article { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double Price { get; set; }
+
+
+        public double CostCaculation(bool needEdv)
+        {
+            if (needEdv==true)
+            {
+                return Price * (double)Quantity + Price * 0.18;
+
+            }
+            else
+            {
+                return Price * (double)Quantity;
+            }
+        }
+
+
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice inv = new Invoice(678904, "Alex", "Foxtrot");
+            inv.Article = "USB-hab";
+            inv.Quantity = 6;
+            inv.Price = 30;
+            Console.WriteLine(inv.CostCaculation(true));
+        }
+    }
+}
